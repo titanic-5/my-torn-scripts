6c6a653e89e932963f6fffedf3866185e0854f49
titanic-5 <145130511+titanic-5@users.noreply.github.com>
2024-06-11 06:53:15 +0100

    Update Torn Bazaar Auto-Max Buy.js

--- Torn Bazaar Auto-Max Buy.js
+++ Torn Bazaar Auto-Max Buy.js
@@ -1,18 +1,19 @@
 // ==UserScript==
 // @name         Torn Bazaar Auto-Max Buy
 // @namespace    Titanic_
-// @version      v1.0
+// @version      v1.3
 // @description  When you click the shopping cart button, quantity will be set to max you can afford.
 // @license      MIT
 // @author       Titanic_ [2968477]
 // @match        https://www.torn.com/bazaar*
 // @grant        none
 // ==/UserScript==
+
 function createListeners() {
     $("#bazaarRoot").on("click", "[class^='controlPanel_'] button", function(e) {
         const item = $(this).closest("[class^=item_]");
 
-        waitForElementToExist("[class^=numberInput").then(() => {
+        waitForElementToExist("[class^=numberInput]").then(() => {
             setTimeout(() => setMaxQuantity(item), 100);
         });
     });
@@ -22,20 +23,27 @@ function setMaxQuantity(item) {
     let wallet = parseInt($("#user-money").text().replace(/[^0-9]/g, ''), 10);
 
     const input = $(item).find("[class^=numberInput]");
-    const price = $(item).find("[class^=price_]").text().replace(/[^0-9]/g, '');
-    const maxqt = $(item).find("[class^=amount_]").text().match(/\(([\d,]+)/)[1].replace(/[^0-9]/g, '');
+    const price = parseInt($(item).find("[class^=price_]").text().replace(/[^0-9]/g, ''), 10);
+    const maxqt = parseInt($(item).find("[class^=amount_]").text().match(/\(([\d,]+)/)[1].replace(/[^0-9]/g, ''), 10);
 
-    let newqt = 0;
-    while(newqt < maxqt) {
-        wallet -= price;
-        if(wallet >= 0) {
-            newqt++;
-        } else {
-            break;
-        }
+    let newqt = Math.floor(wallet / price);
+    newqt = Math.min(newqt, maxqt);
+
+    setNativeValue(input[0], newqt);
+    input[0].dispatchEvent(new Event('input', { bubbles: true }));
+    input[0].dispatchEvent(new Event('change', { bubbles: true }));
+}
+
+function setNativeValue(element, value) {
+    const valueSetter = Object.getOwnPropertyDescriptor(element, 'value').set;
+    const prototype = Object.getPrototypeOf(element);
+    const prototypeValueSetter = Object.getOwnPropertyDescriptor(prototype, 'value').set;
+
+    if (valueSetter && valueSetter !== prototypeValueSetter) {
+        prototypeValueSetter.call(element, value);
+    } else {
+        valueSetter.call(element, value);
     }
-
-    $(input).val(newqt).trigger("input");
 }
 
 function waitForElementToExist(selector) {
