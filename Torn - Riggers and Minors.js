4399bec5505ecd884afa8150357c41732bb5e771
titanic-5 <145130511+titanic-5@users.noreply.github.com>
2024-10-31 17:50:04 +0000

    Update Torn - Riggers and Minors.js

--- Torn - Riggers and Minors.js
+++ Torn - Riggers and Minors.js
@@ -1,10 +1,9 @@
 // ==UserScript==
 // @name         Torn - Riggers and Minors
 // @namespace    Titanic
-// @version      1.0
+// @version      1.1
 // @description  Highlight oil riggers on Torn
 // @author       Titanic
-// @license      MIT
 // @match        https://www.torn.com/page.php?sid=UserList*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -12,6 +11,9 @@
 // ==/UserScript==
 
 function checkJob(element) {
+    //const hasBazaar = checkBazaar(element)
+    //if(hasBazaar) return
+
     const iconTray = element[0].querySelector("span.user-icons > span.icons-wrap > ul#iconTray");
     if (!iconTray) return
 
@@ -24,10 +26,18 @@ function checkJob(element) {
     const titleAttr = parentElement.getAttribute('title');
     if(!titleAttr) return;
 
-    if(titleAttr.includes("Oil Rig") || titleAttr.includes("Mining Corporation")) {
+    if(titleAttr.includes("Oil Rig")) {
         element[0].style.backgroundColor = "rebeccapurple";
-        element[0].style.border = "1px solid red";
     }
+}
+
+function checkBazaar(element) {
+    const bazaar = element[0].querySelector('a[href*="/bazaar"]')
+    if (!bazaar) return false
+
+    element[0].style.backgroundColor = "darkred";
+
+    return true
 }
 
 waitForKeyElements("li[class^='user']", checkJob);
