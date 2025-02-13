e870c3d1a89f3a58c558fc7d5b765e25571945e1
titanic-5 <145130511+titanic-5@users.noreply.github.com>
2025-02-13 22:49:28 +0000

    Update hospital-remove_revives_below_percent.user.js

--- hospital-remove_revives_below_percent.user.js
+++ hospital-remove_revives_below_percent.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove Revives Below X%
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  try to take over the world!
 // @author       Titanic_
 // @match        https://www.torn.com/hospital*
@@ -10,21 +10,22 @@
 
 (function () {
   "use strict";
-
-  let minChance = localStorage.getItem("RRBX_chance") || 80;
-
-  document.querySelector("#top-page-links-list").appendChild(
-    Object.assign(document.createElement("button"), {
-      innerHTML: "% Setting",
-      className: "t-clear h c-pointer  m-icon line-h24 right last",
-      onclick: () => {
-        minChance = prompt("Minimum revive chance to keep?", minChance);
-        localStorage.setItem("RRBX_chance", minChance);
-      },
-    })
-  );
-
   setInterval(() => {
+    let minChance = localStorage.getItem("RRBX_chance") || 80;
+
+    if (!document.querySelector(".rrbx")) {
+      document.querySelector("#top-page-links-list").appendChild(
+        Object.assign(document.createElement("button"), {
+          innerHTML: "% Setting",
+          className: "rrbx t-clear h c-pointer  m-icon line-h24 right last",
+          onclick: () => {
+            minChance = prompt("Minimum revive chance to keep?", minChance);
+            localStorage.setItem("RRBX_chance", minChance);
+          },
+        })
+      );
+    }
+
     let rev = document.querySelectorAll("div.ajax-action");
     if (!rev.length) return;
 
